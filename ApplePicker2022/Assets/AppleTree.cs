fd18f3f92fb5e37b709fc8932532c723207aec67
Nicholas Ward <nicholas.ward2020@gmail.com>
2022-09-13 20:27:26 -0400

    Checkin 09132022

--- ApplePicker2022/Assets/AppleTree.cs
+++ ApplePicker2022/Assets/AppleTree.cs
@@ -20,17 +20,50 @@ public class AppleTree : MonoBehaviour
     //Seconds between Apple Instantiations
     public float appleDropDelay = 1f;
 
-
     // Start is called before the first frame update
     void Start()
     {
         // start dropping apples
+        Invoke("DropApple", 2f);
+
+    }
+
+    // void = signifies return type; default void returns nothing
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
         // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
         // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //move right
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //move left
+        } else if (Random.value < ChangeDirChance)
+        {
+            speed *= -1;
+        }
+        Debug.Log(pos.x);
+    }
+
+    private void FixedUpdate()
+    {
+        // max 50fps
+        if (Random.value < ChangeDirChance)
+        {
+            speed *= -1;
+        }
     }
 }
---
+++ ApplePicker2022/Assets/Basket.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the current screen position of the mouse from Input
+        Vector3 mousePos2d = Input.mousePosition;
+        // the camera's z position sets how far to push the mouse into 3d
+        // if this line causes a NullReferenceException,
+        // select the Main Camera in the hierarchy and set its tag to MainCamera
+        mousePos2d.z = -Camera.main.transform.position.z;
+        // Convert the point from 2d space to 3d world
+        Vector3 mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2d);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        this.transform.position = pos;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // find out what hit this basket
+        GameObject collidedWith = collision.gameObject;
+        
+        if (collidedWith.CompareTag("Apple"))
+        {
+            Destroy(collidedWith);
+        }
+    }
+}
